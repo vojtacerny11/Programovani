f4a1f360f488024e0d3a672695379525e840f274
vojtacerny11 <extycz11@gmail.com>
2023-11-10 17:12:21 +0100

    add IfStatementsScan

---
+++ IfStatementsScan/IfStatementsScan.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int A;
+	int B;
+	printf("zadejte 1. cislo: ");
+	scanf("%d", &A);
+	printf("zadejte 2. cislo: ");
+	scanf("%d", &B);
+	printf("definoval jsem cisla: %d, %d\n", A, B);
+	if (A > B) {
+		printf("maximum je %d", A);
+	}
+	else if (A < B) {
+		printf("Maximum je %d", B);
+	}
+	else {
+		printf("Obe cisla jsou stejna");
+	}
+
+	return 0;
+}

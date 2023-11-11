8db53717bb81a84d2afc41fde84f790360287bd6
vojtacerny11 <extycz11@gmail.com>
2023-11-11 11:40:28 +0100

    update IfStatementsScan float

--- IfStatementsScan/IfStatementsScan.cpp
+++ IfStatementsScan/IfStatementsScan.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-	int A;
+	/*int A;
 	int B;
 	printf("zadejte 1. cislo: ");
 	scanf("%d", &A);
@@ -15,6 +15,23 @@ int main()
 	}
 	else if (A < B) {
 		printf("Maximum je %d", B);
+	}
+	else {
+		printf("Obe cisla jsou stejna");
+	}
+	*/
+	float A;
+	float B;
+	printf("zadejte 1. cislo: ");
+	scanf("%f", &A);
+	printf("zadejte 2. cislo: ");
+	scanf("%f", &B);
+	printf("definoval jsem cisla: %.3f, %.3f\n", A, B);
+	if (A > B) {
+		printf("maximum je %.3f", A);
+	}
+	else if (A < B) {
+		printf("Maximum je %.3f", B);
 	}
 	else {
 		printf("Obe cisla jsou stejna");

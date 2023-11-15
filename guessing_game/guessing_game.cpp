b46e1de724d79cf4442f7699b4bc474ab5873d4f
vojtacerny11 <extycz11@gmail.com>
2023-11-15 17:13:13 +0100

    add guessing_game

---
+++ guessing_game/guessing_game.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int odpoved = 2;
+	int typ = 0;
+	int pokusyLimit = 3;
+	int pokusy = 0;
+	int trueLimit = 0;
+
+	while (typ != odpoved && trueLimit == 0) {
+		if (pokusy < pokusyLimit) {
+			printf("zadejte cislo od 1 do 10: ");
+			scanf("%d", &typ);
+			pokusy++;
+		}
+		else {
+			trueLimit = 1;
+		}
+	} if (trueLimit == 1) {
+		printf("dosly Vam pokusy, spravne cislo bylo %d", odpoved);
+	}
+	else {
+		printf("spravne");
+	}
+
+	return 0;
+}

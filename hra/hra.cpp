3bce9564b46b38f8e2ec5c00480c2001d761e10d
vojtacerny11 <extycz11@gmail.com>
2023-12-11 21:45:00 +0100

    add hra

---
+++ hra/hra.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#define smile 10
+
+char board[3][3];
+int a;
+int b;
+void cislo()
+{
+	printf("napiste cislo a ja ho uhadnu: \n");
+	scanf("%d", &a);
+}
+
+void proces()
+{
+	for (int i = 0; i <= 3; i++)
+	{
+		for (int j = 0; j < 500000000; j++)
+		{
+			int x = j;
+		} 
+		printf(". ");
+		printf(". ");
+	}
+}
+
+void cislo2()
+{
+	printf("myslis si cislo %d", a);
+}
+
+int main()
+{
+	cislo();
+
+	proces();
+
+	printf("\n");
+
+	cislo2();
+
+
+	return 0;
+}

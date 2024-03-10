55f5631f71373e2d67383a40f66071ce7bd2b4a2
vojtacerny11 <extycz11@gmail.com>
2024-03-10 13:24:52 +0100

    update TTCskola 3

--- TicTacToeSkola/TicTacToe.cpp
+++ TicTacToeSkola/TicTacToe.cpp
@@ -10,171 +10,220 @@ void urceniVyherce();
 
 char arr[3][3] = {};
 char sloupec;
-int radek;
 char vyherce = ' ';
+int radek;
 int volnaMista = 9;
+int novaHra = 0;
+bool spatnyVstup = 0;
 
 int main()
 {
-	resetPole();
-	VypisPole();
-
-	while (vyherce == ' ' && volnaMista > 0)
+	while (1)
 	{
-		Hrac1();
+		resetPole();
 		VypisPole();
-		urceniVyherce();
-		if (vyherce != ' ' || volnaMista == 0)
-		{
-			break;
-		}
-		Hrac2();
-		VypisPole();
-		urceniVyherce();
-		if (vyherce != ' ' || volnaMista == 0)
-		{
-			break;
-		}
-	}
-	if (vyherce != ' ')
-	{
-		printf("VYHERCE JE HRAC SE ZNAKEM %c", vyherce);
-	}
-	else
-	{
-		printf("HRA SKONCILA REMIZOU");
-	}
 
-	return 0;
+		while (vyherce == ' ' && volnaMista > 0)
+		{
+			Hrac1();
+			VypisPole();
+			urceniVyherce();
+			if (vyherce != ' ' || volnaMista == 0)
+			{
+				break;
+			}
+			Hrac2();
+			VypisPole();
+			urceniVyherce();
+			if (vyherce != ' ' || volnaMista == 0)
+			{
+				break;
+			}
+		}
+		if (vyherce != ' ')
+		{
+			printf("VYHERCE JE HRAC SE ZNAKEM %c\n", vyherce);
+		}
+		else
+		{
+			printf("HRA SKONCILA REMIZOU\n");
+		}
+		printf("Chcete hrat znovu? Ano: 1 Ne: 0\n");
+		scanf("%d", &novaHra);
+		if (novaHra == 0)
+		{
+			return 0;
+		}
+		else if (novaHra == 1)
+		{
+			vyherce = ' ';
+			volnaMista = 9;
+		}
+	}
 }
 
 void Hrac1()
 {
-	printf("Na tahu je X, zadejte pozici: \n");
-	scanf(" %c%d", &sloupec, &radek);
-	sloupec = tolower(sloupec);
-	radek--;
-	switch (sloupec)
+	do
 	{
-	case 'a':
-		for (int i = 0; i <= radek; i++)
+		printf("Na tahu je X, zadejte pozici: \n");
+		scanf(" %c%d", &sloupec, &radek);
+		sloupec = tolower(sloupec);
+		radek--;
+		switch (sloupec)
 		{
-			if (i == radek)
+		case 'a':
+			for (int i = 0; i <= radek; i++)
 			{
-				if (arr[i][0] == 'O')
+				if (i == radek)
 				{
-					printf("spatny vstup\n");
+					if (arr[i][0] != ' ')
+					{
+						printf("spatny vstup\n");
+						spatnyVstup = 1;
+					}
+					else
+					{
+						arr[i][0] = 'X';
+						spatnyVstup = 0;
+					}
 				}
-				arr[i][0] = 'X';
 			}
-		}
-		break;
-	case 'b':
-		for (int i = 0; i <= radek; i++)
-		{
-			if (i == radek)
+			break;
+		case 'b':
+			for (int i = 0; i <= radek; i++)
 			{
-				if (arr[i][1] == 'O')
+				if (i == radek)
 				{
-					printf("spatny vstup\n");
+					if (arr[i][1] != ' ')
+					{
+						printf("spatny vstup\n");
+						spatnyVstup = 1;
+					}
+					else
+					{
+						arr[i][1] = 'X';
+						spatnyVstup = 0;
+					}
 				}
-				arr[i][1] = 'X';
 			}
-		}
-		break;
-	case 'c':
-		for (int i = 0; i <= radek; i++)
-		{
-			if (i == radek)
+			break;
+		case 'c':
+			for (int i = 0; i <= radek; i++)
 			{
-				if (arr[i][2] == 'O')
+				if (i == radek)
 				{
-					printf("spatny vstup\n");
+					if (arr[i][2] != ' ')
+					{
+						printf("spatny vstup\n");
+						spatnyVstup = 1;
+					}
+					else
+					{
+						arr[i][2] = 'X';
+						spatnyVstup = 0;
+					}
 				}
-				arr[i][2] = 'X';
 			}
+			break;
 		}
-		break;
-	}
+	} while (spatnyVstup == 1);
 	volnaMista--;
 }
 
 void Hrac2()
 {
-	printf("Na tahu je O, zadejte pozici: \n");
-	scanf(" %c%d", &sloupec, &radek);
-	sloupec = tolower(sloupec);
-	radek--;
-	switch (sloupec)
-	{
-	case 'a':
-		for (int i = 0; i <= radek; i++)
+	do {
+		printf("Na tahu je O, zadejte pozici: \n");
+		scanf(" %c%d", &sloupec, &radek);
+		sloupec = tolower(sloupec);
+		radek--;
+		switch (sloupec)
 		{
-			if (i == radek)
+		case 'a':
+			for (int i = 0; i <= radek; i++)
 			{
-				if (arr[i][0] == 'X')
+				if (i == radek)
 				{
-					printf("spatny vstup\n");
+					if (arr[i][0] != ' ')
+					{
+						printf("spatny vstup\n");
+						spatnyVstup = 1;
+					}
+					else
+					{
+						arr[i][0] = 'O';
+						spatnyVstup = 0;
+					}
 				}
-				arr[i][0] = 'O';
-			}
-		} break;
-	case 'b':
-		for (int i = 0; i <= radek; i++)
-		{
-			if (i == radek)
+			} break;
+		case 'b':
+			for (int i = 0; i <= radek; i++)
 			{
-				if (arr[i][1] == 'X')
+				if (i == radek)
 				{
-					printf("spatny vstup\n");
+					if (arr[i][1] != ' ')
+					{
+						printf("spatny vstup\n");
+						spatnyVstup = 1;
+					}
+					else
+					{
+						arr[i][1] = 'O';
+						spatnyVstup = 0;
+					}
 				}
-				arr[i][1] = 'O';
-			}
-		} break;
-	case 'c':
-		for (int i = 0; i <= radek; i++)
-		{
-			if (i == radek)
+			} break;
+		case 'c':
+			for (int i = 0; i <= radek; i++)
 			{
-				if (arr[i][2] == 'X')
+				if (i == radek)
 				{
-					printf("spatny vstup\n");
+					if (arr[i][2] != ' ')
+					{
+						printf("spatny vstup\n");
+						spatnyVstup = 1;
 
+					}
+					else 
+					{
+						arr[i][2] = 'O';
+						spatnyVstup = 0;
+					}
 				}
-				arr[i][2] = 'O';
-			}
-		} break;
-	}
+			} break;
+		}
+	} while (spatnyVstup == 1);
 	volnaMista--;
 }
 
 void urceniVyherce()
 {
-	//rady
-	for (int i = 0; i < 3; i++)
-	{
-		if (arr[i][0] == arr[i][1] && arr[i][0] == arr[i][2])
+		//rady
+		for (int i = 0; i < 3; i++)
 		{
-			vyherce = arr[i][0];
+			if (arr[i][0] == arr[i][1] && arr[i][0] == arr[i][2])
+			{
+				vyherce = arr[i][0];
+			}
 		}
-	}
-	//slopupce
-	for (int i = 0; i < 3; i++)
-	{
-		if (arr[0][i] == arr[1][i] && arr[0][i] == arr[2][i])
+		//slopupce
+		for (int i = 0; i < 3; i++)
 		{
+			if (arr[0][i] == arr[1][i] && arr[0][i] == arr[2][i])
+			{
 				vyherce = arr[0][i];
+			}
 		}
-	}
-	//diagonalni
-	if (arr[0][0] == arr[1][1] && arr[0][0] == arr[2][2])
-	{
-		vyherce = arr[0][0];
-	}
-	if (arr[2][0] == arr[1][1] && arr[2][0] == arr[0][2])
-	{
-		vyherce = arr[2][0];
-	}
+		//diagonalni
+		if (arr[0][0] == arr[1][1] && arr[0][0] == arr[2][2])
+		{
+			vyherce = arr[0][0];
+		}
+		if (arr[2][0] == arr[1][1] && arr[2][0] == arr[0][2])
+		{
+			vyherce = arr[2][0];
+		}
 }
 
 void resetPole()
@@ -193,7 +242,7 @@ void VypisPole()
 	puts("-------------");
 	for (int i = 2; i >= 0; i--)
 	{
-		printf("| %1c | %1c | %1c |\n", arr[i][0], arr[i][1], arr[i][2]);
+		printf("| %c | %c | %c |\n", arr[i][0], arr[i][1], arr[i][2]);
 		puts("-------------");
 	}
 }

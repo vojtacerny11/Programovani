1cd89310d075bc31a9accbeb56f9d1606de0e7f0
vojtacerny11 <extycz11@gmail.com>
2024-04-04 16:16:54 +0200

    add Palindrom

---
+++ PalindromFunkce/palindrom.cpp
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+char str[100];
+bool Pravda = 1;
+int Length;
+
+int DelkaRetezce(const char* s)
+{
+	int ret = 0;
+	while (s[ret] != '\n')
+	{
+		ret++;
+	}
+	return ret;
+}
+
+void KontrolaPalindromu();
+
+int main()
+{
+	fgets(str, 100, stdin);
+	Length = DelkaRetezce(str);
+	KontrolaPalindromu();
+	if (Pravda)
+	{
+		printf("retezec %s je palindrom", str);
+	}
+	else
+	{
+		printf("retezec %s neni palindrom", str);
+	}
+	return 0;
+}
+
+void KontrolaPalindromu()
+{
+	for (int i = 0; i < Length; i++)
+	{
+		if (str[i] != str[Length - 1 - i])
+		{
+			Pravda = 0;
+		}
+	}
+}
